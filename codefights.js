25eae0e13a26a39a0db7d2b14aee968c9a20c3a5
Vaughn Walters <vaughnwalters@gmail.com>
2016-12-10 21:44:22 -0600

    arcade - Seats in Theater

--- codefights.js
+++ codefights.js
@@ -1,13 +1,14 @@
-// n children have got m pieces of candy. They want to eat as much candy as they can, but each child must eat exactly the same amount of candy as any other child. Determine how many pieces of candy will be eaten by all the children together. Individual pieces of candy cannot be split.
+// Your friend advised you to see a new performance in the most popular theater in the city. He knows a lot about art and his advice is usually good, but not this time: the performance turned out to be awfully dull. It's so bad you want to sneak out, which is quite simple, especially since the exit is located right behind your row to the left. All you need to do is climb over your seat and make your way to the exit.
+
+// The main problem is your shyness: you're afraid that you'll end up blocking the view (even if only for a couple of seconds) of all the people who sit behind you and in your column or the columns to your left. To gain some courage, you decide to calculate the number of such people and see if you can possibly make it to the exit without disturbing too many people.
+
+// Given the total number of rows and columns in the theater (nRows and nCols, respectively), and the row and column you're sitting in, return the number of people who sit strictly behind you and in your column or to the left, assuming all seats are occupied.
 
 // Example
 
-// For n = 3 and m = 10, the output should be
-// candies(n, m) = 9.
+// For nCols = 16, nRows = 11, col = 5 and row = 3, the output should be
+// seatsInTheater(nCols, nRows, col, row) = 96.
 
-// Each child will eat 3 pieces. So the answer is 9.
-
-
-function candies(n, m) {
-    return m - (m % n)
+function seatsInTheater(nCols, nRows, col, row) {
+    return (nRows - row) * (nCols - col + 1)
 }
